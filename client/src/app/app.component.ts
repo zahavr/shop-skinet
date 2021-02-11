5af41c0ca18521c69c6bfcf8c06542078e5f795b
Alexey <alexeywork34@gmail.com>
2021-02-11 17:12:10 +0600

    added nav menu

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+  products: any[];
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((res: any) => {
+      this.products = res.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
